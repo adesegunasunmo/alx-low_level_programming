ec88432d1f30af946fa3391abaf43689dcf8e141
adesegun <adesegunabayomi@gmail.com>
2022-09-09 13:51:04 -0700

    Task 2-3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	char sam;
+
+for (seg = 'a'; seg <= 'z'; seg++)
+{
+	putchar(sam);
+}
+	putchar('\n');
+return (0);
+}
